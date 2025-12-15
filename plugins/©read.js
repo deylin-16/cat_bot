3e8bfe27ea4e616366336cfb199fe291d746e539
deylin-16 <deylin1616@gmail.com>
2025-12-14 21:05:12 -0600

    Actualizar ©read.js

--- plugins/©read.js
+++ plugins/©read.js
@@ -2,16 +2,16 @@ const { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
 
 let handler = async (m, { conn, usedPrefix }) => {
 let quoted = m.quoted
+
 if (!quoted) return 
 
 try {
-  // await m.react('')
-
+  
 let viewOnceMessage = quoted.viewOnce ? quoted : quoted.mediaMessage?.imageMessage || quoted.mediaMessage?.videoMessage || quoted.mediaMessage?.audioMessage
 let messageType = viewOnceMessage.mimetype || quoted.mtype
 let stream = await downloadContentFromMessage(viewOnceMessage, messageType.split('/')[0])    
 if (!stream) {
-    await m.react('✖️')
+    
     return 
 }  
 
@@ -21,35 +21,39 @@ buffer = Buffer.concat([buffer, chunk])
 }
 
 const destinationJid = m.sender 
+const senderName = m.pushName || 'El usuario'
+const captionText = `✅ Contenido de visualización única robado por ${senderName}.` 
 
 if (messageType.includes('video')) {
 await conn.sendMessage(destinationJid, { 
     video: buffer, 
-    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.`, 
+    caption: captionText, 
     mimetype: 'video/mp4' 
-}, { quoted: m }) 
+}) 
 } else if (messageType.includes('image')) {
 await conn.sendMessage(destinationJid, { 
     image: buffer, 
-    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.` 
-}, { quoted: m })
+    caption: captionText 
+})
 } else if (messageType.includes('audio')) {
 await conn.sendMessage(destinationJid, { 
     audio: buffer, 
-    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.`,
+    caption: captionText,
     mimetype: 'audio/ogg; codecs=opus', 
     ptt: viewOnceMessage.ptt || false 
-}, { quoted: m })  
+})  
+} else {
+    
+    return
 }
 
-//await m.react('✔️')
-
 } catch (e) {
-    await m.react('✖️')
-    console.error('Error al robar ViewOnce:', e)
+    
+    console.error('Error al robar ViewOnce en modo secreto:', e)
 }}
 
 
-handler.command = ['🫦'] 
+handler.command = ['👍', '👁️', '👁️‍🗨️'] 
+
 
 export default handler

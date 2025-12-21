e46f0025de7acfa53459ec4511f8d1d04db0b6ff
deylin-16 <deylin1616@gmail.com>
2025-12-21 13:32:53 -0600

    Actualizar ©get.js

--- plugins/©get.js
+++ plugins/©get.js
@@ -1,25 +1,28 @@
-import fetch from 'node-fetch';
-import {format} from 'util';
-const handler = async (m, {text}) => {
-    if (!/^https?:\/\//.test(text)) return m.reply(' Te faltó el *url* de la pagina.');
-  const _url = new URL(text);
-  const url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY');
-  const res = await fetch(url);
-  if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-    
-    throw `Content-Length: ${res.headers.get('content-length')}`;
-  }
-  if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m);
-  let txt = await res.buffer();
-  try {
-    txt = format(JSON.parse(txt + ''));
-  } catch (e) {
-    txt = txt + '';
-  } finally {
-    m.reply(txt.slice(0, 655366666) + '');
-  }
-};
+import fetch from 'node-fetch'
+import { format } from 'util'
 
-handler.command = ['get'];
+let handler = async (m, { conn, text }) => {
+if (m.fromMe) return
+if (!/^https?:\/\//.test(text)) return m.reply(` Por favor, ingresa la *url* de la pagina.`)
+let url = text
+await m.react('🕒')
+let res = await fetch(url)
+if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+throw `Content-Length: ${res.headers.get('content-length')}`
+}
+if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+let txt = await res.buffer()
+try {
+txt = format(JSON.parse(txt + ''))
+} catch (e) {
+txt = txt + ''
+} finally {
+m.reply(txt.slice(0, 65536) + '')
+await m.react('✔️')
+}}
 
-export default handler;
+
+handler.command = ['get']
+
+
+export default handler

deeb81c87489617f070546ac20996fae9e6435b2
deylin-16 <deylin1616@gmail.com>
2025-12-28 18:34:41 -0600

    Actualizar ©get.js

--- plugins/©get.js
+++ plugins/©get.js
@@ -1,33 +1,16 @@
 import fetch from 'node-fetch'
 import { format } from 'util'
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, usedPrefix, text }) => {
 if (m.fromMe) return
-if (!/^https?:\/\//.test(text)) return m.reply(`Por favor, ingresa la *url* de la pagina.`)
-
+if (!/^https?:\/\//.test(text)) return m.reply(` Por favor, ingresa la *url* de la pagina.`)
 let url = text
 await m.react('🕒')
-
-try {
-let res = await fetch(url, {
-headers: {
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+let res = await fetch(url)
+if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+throw `Content-Length: ${res.headers.get('content-length')}`
 }
-})
-
-let contentLength = res.headers.get('content-length')
-if (contentLength && parseInt(contentLength) > 200 * 1024 * 1024) {
-return m.reply(`El archivo es demasiado pesado para enviarlo.`)
-}
-
-let contentType = res.headers.get('content-type')
-
-if (!/text|json/.test(contentType)) {
-await conn.sendFile(m.chat, url, 'archivo', text, m)
-await m.react('✔️')
-return
-}
-
+if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
 let txt = await res.buffer()
 try {
 txt = format(JSON.parse(txt + ''))
@@ -36,13 +19,8 @@ txt = txt + ''
 } finally {
 m.reply(txt.slice(0, 65536) + '')
 await m.react('✔️')
-}
-} catch (e) {
-console.error(e)
-m.reply(`Error al intentar obtener el archivo.`)
-}
-}
+}}
 
-handler.command = ['get']
+handler.command = ['fetch', 'get']
 
 export default handler

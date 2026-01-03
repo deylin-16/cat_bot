417a1b2f9e3040f08085724f577bd946b9280896
deylin-16 <deylin1616@gmail.com>
2026-01-03 13:03:32 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,24 +1,24 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+
 let handler = async (m, { conn }) => {
-    let chats = global.db.data.chats
-    let count = 0
-
-    Object.keys(chats).forEach(jid => {
-        if (chats[jid].primaryBot) {
-            chats[jid].primaryBot = ''
-            count++
+    
+    await conn.sendMessage(m.chat, {
+        text: "https://hola.com//",
+        contextInfo: {
+            externalAdReply: {
+                title: "HOLA.com, últimas noticias de famosos, moda, belleza y actualidad",
+                body: "Número 1 en actualidad y tendencias de moda, belleza y estilo de vida. Noticias diarias sobre las estrellas de cine, la música, tendencias de moda, consejos de belleza, recetas de cocina, estilo de vida y la actualidad de las principales casas reales del mundo.",
+                previewType: "NONE",
+                thumbnailUrl: "https://ik.imagekit.io/pm10ywrf6f/dynamic_Bot_by_deylin/1767466951467_BNENXDN6p.jpeg", // Puedes cambiar por una URL de imagen real
+                sourceUrl: "https://hola.com//",
+                mediaType: 1,
+                renderLargerThumbnail: true
+            }
         }
-    })
-
-    if (count === 0) {
-        return m.reply('✅ No había ningún grupo con prioridad establecida. Todos los bots ya estaban libres.')
-    }
-
-    await conn.sendMessage(m.chat, { 
-        text: `✅ *Restauración Completa*\n\nSe ha eliminado la prioridad en *${count}* grupos. Ahora todos los asistentes responderán en todas partes.` 
     }, { quoted: m })
 }
 
-handler.command = /^(restaurarbot|resetbots|liberartodo)$/i
-handler.rowner = true
+handler.command = /^(prueba)$/i
 
 export default handler

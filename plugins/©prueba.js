7d2826af605ecb7dc82abbce320104749def0724
deylin-16 <deylin1616@gmail.com>
2026-01-03 13:20:09 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -1,27 +1,25 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-    
-    let imagenUrl = "https://ik.imagekit.io/pm10ywrf6f/dynamic_Bot_by_deylin/1767466951467_BNENXDN6p.jpeg" // Pon aquí la URL de la imagen
-    let response = await fetch(imagenUrl)
-    let bufferImagen = await response.buffer()
+    const imageUrl = "https://ik.imagekit.io/pm10ywrf6f/dynamic_Bot_by_deylin/1767466951467_BNENXDN6p.jpeg"
+    const response = await fetch(imageUrl)
+    const buffer = await response.buffer()
 
-    await conn.sendMessage(m.chat, {
-        text: "https://hola.com//",
-        contextInfo: {
-            externalAdReply: {
-                title: "HOLA.com, últimas noticias de famosos, moda, belleza y actualidad",
-                body: "Número 1 en actualidad y tendencias de moda, belleza y estilo de vida.",
-                previewType: "NONE",
-                mediaType: 1,
-                renderLargerThumbnail: true,
-                thumbnail: bufferImagen, // Al ser Buffer, el clic no va a la imagen
-                sourceUrl: "https://hola.com//" // El clic ahora irá siempre aquí
-            }
+    const messageStruct = {
+        extendedTextMessage: {
+            text: "https://hola.com//",
+            matchedText: "https://hola.com//",
+            description: "Número 1 en actualidad y tendencias de moda, belleza y estilo de vida. Noticias diarias sobre las estrellas de cine, la música, tendencias de moda, consejos de belleza, recetas de cocina, estilo de vida y la actualidad de las principales casas reales del mundo.",
+            title: "HOLA.com, últimas noticias de famosos, moda, belleza y actualidad",
+            previewType: "NONE",
+            jpegThumbnail: buffer,
+            inviteLinkGroupTypeV2: "DEFAULT"
         }
-    }, { quoted: m })
+    }
+
+    await conn.relayMessage(m.chat, messageStruct, { quoted: m })
 }
 
-handler.command = /^(prueba)$/i
+handler.command = ['prueba']
 
 export default handler

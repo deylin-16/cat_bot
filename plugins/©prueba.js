1998869c92188cd185104ec1b39d73351c9b7cac
deylin-16 <deylin1616@gmail.com>
2026-01-03 13:24:09 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
 
     const messageStruct = {
         extendedTextMessage: {
-            text: "hola jajaja",
+            text: "https://hola.com// hola jajaja",
             matchedText: "https://hola.com//",
             description: "Número 1 en actualidad y tendencias de moda, belleza y estilo de vida. Noticias diarias sobre las estrellas de cine, la música, tendencias de moda, consejos de belleza, recetas de cocina, estilo de vida y la actualidad de las principales casas reales del mundo.",
             title: "HOLA.com, últimas noticias de famosos, moda, belleza y actualidad",

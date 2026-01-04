ae75fc4573ececd1fc200cf5f492e5ae85b3b966
deylin-16 <deylin1616@gmail.com>
2026-01-04 16:00:31 -0600

    Actualizar ©stickers.js

--- plugins/©stickers.js
+++ plugins/©stickers.js
@@ -17,7 +17,7 @@ if (!img) return conn.reply(m.chat, `🍪 *_La conversión ha fallado, intenta e
 
 let out
 try {
-stiker = await sticker(img, false, global.packsticker, global.author)
+stiker = await sticker(img, false, `BOTA: ${global.name(con)}`)
 } catch (e) {
 console.error(e)
 } finally {
@@ -26,7 +26,7 @@ if (/webp/g.test(mime)) out = await webp2png(img)
 else if (/image/g.test(mime)) out = await uploadImage(img)
 else if (/video/g.test(mime)) out = await uploadFile(img)
 if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, global.packsticker, global.author)
+stiker = await sticker(false, out, `BOTA: ${global.name(con)}`)
 }}
 } else if (args[0]) {
 if (isUrl(args[0])) stiker = await sticker(false, args[0], `BOTA: ${global.name(con)}`)

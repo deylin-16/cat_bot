91bf4451559a2a8d326a35ef422a7514ab113e07
deylin-16 <deylin1616@gmail.com>
2026-01-08 17:55:55 -0600

    Actualizar ©acceso.js

--- plugins/©acceso.js
+++ plugins/©acceso.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, command }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363406846602793@newsletter',
-                    newsletterName: `SIGUE EL CANAL DE: ${conn.user.name || 'Bot'}`,
+                    newsletterName: `SIGUE EL CANAL DE: ${name(conn) || 'Bot'}`,
                     serverMessageId: 1
                 },
                 externalAdReply: {

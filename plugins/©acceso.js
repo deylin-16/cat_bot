7cae0547e98dfaf202c181efeb892c409db8d65b
deylin-16 <deylin1616@gmail.com>
2026-01-07 18:00:51 -0600

    Actualizar ©acceso.js

--- plugins/©acceso.js
+++ plugins/©acceso.js
@@ -33,7 +33,7 @@ let handler = async (m, { conn, command }) => {
                     title: 'VINCULAR SUB-BOT',
                     body: 'dynamic bot pairing code - ',
                     thumbnailUrl: 'https://ik.imagekit.io/pm10ywrf6f/dynamic_Bot_by_deylin/1767826205356_ikCIl9sqp0.jpeg',
-                    mediaType: 2,
+                    mediaType: 1,
                     mediaUrl: url,
                     sourceUrl: url,
                     renderLargerThumbnail: true

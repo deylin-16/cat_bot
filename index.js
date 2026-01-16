91d6b2d4880c1d9423d9958bc667e3dfe26edf46
deylin-16 <deylin1616@gmail.com>
2026-01-16 14:57:47 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
+Process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
 import './config.js';
 import { platform } from 'process';
 import { fileURLToPath, pathToFileURL } from 'url';

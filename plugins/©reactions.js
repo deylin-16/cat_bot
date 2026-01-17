05b6bfb0ae2aa3f59107298898b2e0645caae3c8
deylin-16 <deylin1616@gmail.com>
2026-01-16 20:26:32 -0600

    Actualizar ©reactions.js

--- plugins/©reactions.js
+++ plugins/©reactions.js
@@ -53,7 +53,6 @@ let handler = async (m, { conn, command }) => {
     let data = dbReacciones[key]
     if (!data) return
 
-    
     if (!data.wa_ids) data.wa_ids = []
 
     await conn.sendMessage(m.chat, { react: { text: data.emoji, key: m.key } })
@@ -74,26 +73,26 @@ let handler = async (m, { conn, command }) => {
         textoFinal = data.txt_grupo.replace('@user', `@${user.split('@')[0]}`)
     }
 
-    
     try {
-        
         if (data.wa_ids.length >= 5) {
-            let randomId = data.wa_ids[Math.floor(Math.random() * data.wa_ids.length)]
-            await conn.sendMessage(m.chat, { forward: randomId, caption: textoFinal, mentions: menciones }, { quoted: m })
+            let randomMsg = data.wa_ids[Math.floor(Math.random() * data.wa_ids.length)]
+            await conn.sendMessage(m.chat, {
+                video: randomMsg.message.videoMessage,
+                caption: textoFinal,
+                gifPlayback: true,
+                mentions: menciones
+            }, { quoted: m })
             return 
         }
 
-        
         const { data: tenorRes } = await axios.get(
             `https://api.tenor.com/v1/search?q=${encodeURIComponent(data.search)}&key=LIVDSRZULELA&limit=15`
         )
 
         if (!tenorRes?.results?.length) throw new Error()
-
         const randomGif = tenorRes.results[Math.floor(Math.random() * tenorRes.results.length)]
         const videoUrl = randomGif.media[0].mp4.url
 
-        
         let sentMsg = await conn.sendMessage(m.chat, {
             video: { url: videoUrl },
             caption: textoFinal,
@@ -101,17 +100,16 @@ let handler = async (m, { conn, command }) => {
             mentions: menciones
         }, { quoted: m })
 
-        
         data.wa_ids.push(sentMsg) 
         fs.writeFileSync(path, JSON.stringify(dbReacciones, null, 2))
 
     } catch (e) {
         console.error(e)
-        
-        m.reply('❌ Error al procesar la reacción, intentando de nuevo...')
+        m.reply('❌ Error.')
     }
 }
 
-handler.command = /^(beso|kiss[23]?|beso[23]|abrazo|hug|hug2|abrazo2|golpe|slap|matar|kill|pat|acariciar|bailar|dance|patada|kick2|reir|laugh|triste|sad|cry|enojado|angry|saludo|wave|morder|bite|lamer|lick|dormir|sleep|comer|eat|asustar|scare|disparar|shoot|correr|run|mirar|stare|asombro|wow|tímido|blush|cafe|coffee|pizza|ramen|tee|boxeo|gritar|pensar|fumar|jugar|esconderse|suicidio|suicide|ignorar|aplaudir|clap|vomitar|vomit|cocinar|cook|casar|marry|divorcio|divorce|cachetada|twerk|estudiar|study|trabajar|work|ejercicio|workout|bañarse|shower|espiar|stalk|volar|fly|beber|drink|beer|fútbol|soccer|baloncesto|basketball|nadar|swim|cantar|sing|tocar_guitarra|guitar|tocar_piano|piano|dibujar|draw|escribir|write|leer|read|viajar|travel|comprar|shop|limpiar|clean|conducir|drive|montar_bici|bike|teletransportarse|teleport|explotar|explode|congelar|freeze|quemar|burn|electrocutar|lightning|invocar|summon|transformarse|morph|sanar|heal|proteger|protect|caerse|fall|pescar|fish|jardinería|garden|meditar|yoga|apostar|gamble|robar|steal|mentir|lie|asustarse|fear|suplicar|beg|desprecio|disgust|orgulloso|smug|aburrido|bored|confundido|emocionado|excited|avergonzado|cansado|tired|beber_agua|water|comer_hamburguesa|burger|comer_dulces|candy|desayunar|cenar|dinner|comer_helado|icecream|beber_jugo|juice|comer_fruta|fruit|vapear|vape|peinarse|brush|maquillarse|makeup|vestirse|dress|mirarse_espejo|mirror|llorar_risa|dar_dinero|money|pedir_dinero|trabajar_pc|pc|ver_tv|tv|escuchar_musica|music|fotografiar|photo|grabar|record|patinar|skate|surfear|surf|esquiar|ski|acampar|camp|pesadilla|despertar|wakeup|estornudar|sneeze|enfermo|sick|curar|abofetear_trasero|spank|mimar|cuddle|beso_mano|beso_frente|cargar_hombros|pelea_almohadas|pillowfight|jugar_cartas|beber_soda|comer_tacos|comer_chocolate|mirar_estrellas|stars)$/i
+handler.command = /^(beso|kiss[23]?|beso[23]|abrazo|hug|hug2|abrazo2|golpe|slap|matar|kill|pat|acariciar|bailar|dance|patada|kick2|reir|laugh|triste|sad|cry|enojado|angry|saludo|wave|morder|bite|lamer|lick|dormir|sleep|comer|eat|asustar|scare|disparar|shoot|correr|run|mirar|stare|asombro|wow|tímido|blush|cafe|coffee|pizza|ramen|tee|boxeo|gritar|pensar|fumar|jugar|esconderse|suicidio|suicide|ignorar|aplaudir|clap|vomit|vomitar|cook|cocinar|marry|casar|divorce|divorcio|cachetada|twerk|study|estudiar|work|trabajar|workout|ejercicio|shower|bañarse|stalk|espiar|fly|volar|drink|beber|beer|soccer|fútbol|basketball|baloncesto|swim|nadar|sing|cantar|guitar|tocar_guitarra|piano|tocar_piano|draw|dibujar|write|escribir|read|leer|travel|viajar|shop|comprar|clean|limpiar|drive|conducir|bike|montar_bici|teleport|teletransportarse|explode|explotar|freeze|congelar|burn|quemar|lightning|electrocutar|summon|invocar|morph|transformarse|heal|sanar|protect|proteger|fall|caerse|fish|pescar|garden|jardinería|meditate|yoga|gamble|apostar|steal|robar|lie|mentir|fear|asustarse|beg|suplicar|disgust|desprecio|smug|orgulloso|bored|aburrido|confused|confundido|excited|emocionado|avergonzado|tired|cansado|water|beber_agua|burger|comer_hamburguesa|candy|comer_dulces|breakfast|desayunar|dinner|cenar|icecream|helado|juice|jugo|fruit|fruta|vape|vapear|brush|peinarse|makeup|maquillarse|dress|vestirse|mirror|mirarse_espejo|llorar_risa|money|dinero|pc|tv|music|escuchar_musica|photo|fotografiar|record|grabar|skate|patinar|surf|surfear|ski|esquiar|camp|acampar|nightmare|pesadilla|wakeup|despertar|sneeze|estornudar|sick|enfermo|curar|spank|mimar|cuddle|beso_mano|beso_frente|cargar_hombros|pillowfight|pelea_almohadas|stars|mirar_estrellas)$/i
 handler.group = true
+
 export default handler

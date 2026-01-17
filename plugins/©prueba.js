438d1e4db055f1a3c74142398a1d234562362d08
deylin-16 <deylin1616@gmail.com>
2026-01-17 17:13:49 -0600

    Actualizar ©prueba.js

--- plugins/©prueba.js
+++ plugins/©prueba.js
@@ -3,85 +3,47 @@ import { format } from 'util'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import { createRequire } from 'module'
-import fs from 'fs'
-import fetch from 'node-fetch'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
 
 let handler = async (m, _2) => {
   let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
-  
-  // Variables que estarán disponibles directamente en el comando
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
-  
+  let old = m.exp * 1
   try {
-    // Definimos el contexto de ejecución con acceso a global y módulos
-    let exec = new (async () => { }).constructor(
-      'print', 
-      'm', 
-      'handler', 
-      'require', 
-      'conn', 
-      'Array', 
-      'process', 
-      'args', 
-      'groupMetadata', 
-      'module', 
-      'exports', 
-      'argument', 
-      'p', 
-      'fs', 
-      'fetch',
-      'global',
-      _text
-    )
+    let i = 15
+    let f = {
+      exports: {}
+    }
+    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', 'p', _text)
 
     const printFunc = (...args) => {
+      if (--i < 1) return
+      console.log(...args)
       return conn.reply(m.chat, format(...args), m)
-    }
+    };
 
-    // Pasamos las instancias reales para que m.chat o global.db funcionen
-    _return = await exec.call(
-      conn, 
-      printFunc, 
-      m, 
-      handler, 
-      require, 
-      conn, 
-      CustomArray, 
-      process, 
-      args, 
-      groupMetadata, 
-      { exports: {} }, 
-      {}, 
-      [conn, _2], 
-      printFunc, 
-      fs, 
-      fetch,
-      global
-    )
+    _return = await exec.call(conn, printFunc, m, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2], printFunc)
   } catch (e) {
     let err = syntaxerror(_text, 'Execution Function', {
       allowReturnOutsideFunction: true,
       allowAwaitOutsideFunction: true,
-      sourceType: 'module'
+        sourceType: 'module'
     })
     if (err) _syntax = '```' + err + '```\n\n'
     _return = e
   } finally {
-    // Si el comando devuelve algo, lo enviamos al chat
-    if (_return !== undefined) {
-      conn.reply(m.chat, _syntax + format(_return), m)
-    }
+    conn.reply(m.chat, _syntax + format(_return), m)
+    m.exp = old
   }
 }
-
-handler.help = ['=>'] 
+handler.help = ['=> '] 
 handler.tags = ['owner']
-handler.command = ['=>']
+handler.customPrefix = /^=> / 
+handler.command = /(?:)/i 
 handler.rowner = true
 
 export default handler

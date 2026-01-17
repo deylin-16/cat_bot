cd610d2ba1e39cce4c48a5def40c8ad55571bd13
deylin-16 <deylin1616@gmail.com>
2026-01-17 16:45:14 -0600

    Actualizar ©bot_on_off.js

--- plugins/©bot_on_off.js
+++ plugins/©bot_on_off.js
@@ -1,20 +1,15 @@
-let handler = async (m, { conn, command }) => {
-    if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '〽️ *¡Este chat no está registrado!*', m)
-    let chat = global.db.data.chats[m.chat].isBanned = true
-    let isBanning = command === 'banchat'
-    if (isBanning) {
-        if (chat.isBanned) return conn.reply(m.chat, '⚠️ *¡El bot ya estaba baneado!*', m)
-        chat.isBanned = true
-        await conn.reply(m.chat, '🚫 𝗘𝗹 𝗕𝗼𝘁 𝗛𝗮 𝗦𝗶𝗱𝗼 𝗗𝗲𝘀𝗮𝗰𝘁𝗶𝘃𝗮𝗱𝗼', m)
-    } else {
-        if (!chat.isBanned) return conn.reply(m.chat, '👑 *¡El bot no está baneado!*', m)
-        chat.isBanned = false
-        await conn.reply(m.chat, '⚡ *¡El bot ya fue desbaneado!*', m)
-    }
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '〽️l🔥 *¡Este chat no está registrado!*', m)
+let chat = global.db.data.chats[m.chat]
+if (!chat.isBanned) return conn.reply(m.chat, '👑 *¡ᴇʟ ʙᴏᴛ ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m)
+chat.isBanned = false
+await conn.reply(m.chat, '⚡ *¡ᴇʟ ʙᴏᴛ ʏᴀ ғᴜᴇ ᴅᴇsʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m)
 }
-
-handler.command = ['banchat', 'unbanchat', 'desbanearchat', 'desbanchat']
+handler.help = ['unbanchat'];
+handler.tags = ['grupo'];
+handler.command = ['unbanchat','desbanearchat','desbanchat']
 handler.admin = true 
-handler.botAdmin = true
+handler.botadmin = true
 handler.group = true
+
 export default handler

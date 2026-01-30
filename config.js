c6990094b016dee6abd42dc5fad2e648b81ca0f1
deylin-16 <deylin1616@gmail.com>
2026-01-29 23:44:37 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,132 +1,85 @@
-import { watchFile, unwatchFile } from 'fs' 
+import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 import cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import axios from 'axios'
-import moment from 'moment-timezone' 
+import moment from 'moment-timezone'
 import path from 'path'
 
-global.owner = [
-  [ '50432955554', 'Eliac', true ]
-]; 
+global.owner = [['50432955554', 'Eliac', true]]
 
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment 
-global.sessions = 'sessions'
-global.url = 'https://deylin.xyz/pairing_code'
-global.jadi = 'sessions_sub_assistant';
-global.url_api = 'https://api.deylin.xyz'
-global.key = 'dk_ofical_user'
-global.ch = {
-ch1: '120363406846602793@newsletter'
+global.botNames = [
+  'DYNAMIC BOT - V2',
+  'JIJI - ASSISTANT',
+  'DEYLIN - TECH',
+  'MINIMALIST BOT'
+]
+
+global.botImages = [
+  'https://ik.imagekit.io/pm10ywrf6f/dynamic_Bot_by_deylin/1767826205356_ikCIl9sqp0.jpeg',
+  'https://i.ibb.co/g8PsK57/IMG-20251224-WA0617.jpg',
+  'https://ik.imagekit.io/pm10ywrf6f/dynamic_Bot_by_deylin/1768372124872_Wl9aOex7E.jpeg'
+]
+
+const conf = {
+  utils: {
+    cheerio,
+    fs,
+    fetch,
+    axios,
+    moment
+  },
+  api: {
+    url: 'https://api.deylin.xyz',
+    key: 'dk_ofical_user'
+  },
+  sessions: {
+    main: 'sessions',
+    sub: 'sessions_sub_assistant',
+    pairing: 'https://deylin.xyz/pairing_code'
+  },
+  social: {
+    channel: '120363406846602793@newsletter'
+  }
 }
 
-var more = String.fromCharCode(8206)
-global.rmr = more.repeat(850)
+Object.assign(global, conf.utils)
+global.url_api = conf.api.url
+global.key = conf.api.key
+global.sessions = conf.sessions.main
+global.jadi = conf.sessions.sub
+global.url = conf.sessions.pairing
+global.ch = { ch1: conf.social.channel }
 
-global.getAssistantConfig = (botJid) => {
-    const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    const DB_PATH = path.join(__dirname, 'db/assistant_sessions.json')
-    let configs = {}
-    try {
-        if (fs.existsSync(DB_PATH)) {
-            configs = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
-        }
-    } catch (e) { console.error(e) }
-
-    const sessionConfig = configs[botJid]
-    let assistantName = sessionConfig?.assistantName || "JIJI - ASSISTANT"
-    let assistantImage = sessionConfig?.assistantImage 
-        ? Buffer.from(sessionConfig.assistantImage, 'base64') 
-        : 'https://i.ibb.co/g8PsK57/IMG-20251224-WA0617.jpg'
-    let assistantIcon = sessionConfig?.assistantIcon 
-        ? Buffer.from(sessionConfig.assistantIcon, 'base64') 
-        : null
-
-    return { assistantName, assistantImage, assistantIcon }
-}
-
-global.name = (conn) => global.getAssistantConfig(conn.user.jid).assistantName
-global.img = (conn) => global.getAssistantConfig(conn.user.jid).assistantImage
-
-
-    global.design = async (conn, m, text = '') => {
-    const config = global.getAssistantConfig(conn.user.jid)
-    const mainBotJid = global.conn?.user?.jid.split('@')[0] 
-    const currentBotJid = conn.user.jid.split('@')[0]
-
-    if (currentBotJid === mainBotJid) {
-        return await conn.sendMessage(m.chat, { text: text }, { quoted: m })
-    }
-
-    let url = 'https://deylin.xyz/pairing_code?v=5' 
-    let buffer = config.assistantIcon || config.assistantImage
-
-    return await conn.sendMessage(m.chat, {
-        text: text || canalLink,
-        contextInfo: {
-            isForwarded: true,
-            forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363406846602793@newsletter',
-                newsletterName: `SIGUE EL CANAL DE: ${config.assistantName}`,
-                serverMessageId: 1
-            },
-             externalAdReply: {
-              title: config.assistantName,
-              body: config.assistantName,
-              thumbnail: typeof buffer === 'string' ? await global.getBuffer(buffer) : buffer,
-              mediaType: 2,
-              mediaUrl: url,
-              sourceUrl: url
-            }
-        }
-    }, { quoted: m })
-}
-
+global.name = () => global.botNames[Math.floor(Math.random() * global.botNames.length)]
+global.img = () => global.botImages[Math.floor(Math.random() * global.botImages.length)]
 
 global.getBuffer = async (url, options = {}) => {
-    try {
-        var res = await axios({
-            method: "get",
-            url,
-            headers: {
-                'DNT': 1,
-                'User-Agent': 'GoogleBot',
-                'Upgrade-Insecure-Request': 1
-            },
-            ...options,
-            responseType: 'arraybuffer'
-        })
-        return res.data
-    } catch (e) {
-        console.log(`Error : ${e}`)
-    }
+  try {
+    const res = await axios({
+      method: "get",
+      url,
+      headers: {
+        'DNT': 1,
+        'User-Agent': 'GoogleBot'
+      },
+      ...options,
+      responseType: 'arraybuffer'
+    })
+    return res.data
+  } catch (e) {
+    return null
+  }
 }
 
 const d = new Date(new Date().getTime() + 3600000)
-global.d = d
-global.locale = 'es'
-global.dia = d.toLocaleDateString('es', {weekday: 'long'})
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
-global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' })
+global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
 
-let ase = new Date(); 
-let hour = ase.getHours(); 
-let saludo;
-if (hour >= 0 && hour < 3) saludo = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
-else if (hour >= 3 && hour < 7) saludo = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'
-else if (hour >= 7 && hour < 10) saludo = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'
-else if (hour >= 10 && hour < 14) saludo = 'Lɪɴᴅᴏ Dɪᴀ 🌤'
-else if (hour >= 14 && hour < 18) saludo = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'
-else saludo = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
-global.saludo = saludo;
+const hour = new Date().getHours()
+global.saludo = hour < 12 ? 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅' : hour < 18 ? 'Lɪɴᴅᴀ T Tᴀʀᴅᴇ 🌆' : 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {

47211975f5dd0945ce2db0dbb53b13fcba1fe363
deylin-16 <deylin1616@gmail.com>
2026-01-30 14:02:32 -0600

    Actualizar group_settings.js

--- plugins/group/group_settings.js
+++ plugins/group/group_settings.js
@@ -4,29 +4,33 @@ const groupConfig = {
     category: 'group',
     admin: true,
     run: async (m, { conn, text, command }) => {
-        const chat = global.db.data.chats[m.chat]
+        const chat = global.db.data.chats[m.chat];
         
         if (command === 'welcome') {
-            chat.welcome = !chat.welcome
-            m.reply(`┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ ᴡᴇʟᴄᴏᴍᴇ: ${chat.welcome ? 'ᴏɴ' : 'ᴏғғ'}\n┗━━━━━━━━━━━━━━━━━━┛`)
+            chat.welcome = !chat.welcome;
+            await conn.sendMessage(m.chat, { 
+                text: `┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ sᴛᴀᴛᴜs: ᴡᴇʟᴄᴏᴍᴇ\n┃ ✎ sᴛᴀᴛᴇ: ${chat.welcome ? 'ᴀᴄᴛɪᴠᴀᴛᴇᴅ' : 'ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ'}\n┗━━━━━━━━━━━━━━━━━━┛` 
+            }, { quoted: m });
         }
 
         if (command === 'detect') {
-            chat.detect = !chat.detect
-            m.reply(`┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ ᴅᴇᴛᴇᴄᴛᴏʀ: ${chat.detect ? 'ᴏɴ' : 'ᴏғғ'}\n┗━━━━━━━━━━━━━━━━━━┛`)
+            chat.detect = !chat.detect;
+            await conn.sendMessage(m.chat, { 
+                text: `┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ sᴛᴀᴛᴜs: ᴅᴇᴛᴇᴄᴛᴏʀ\n┃ ✎ sᴛᴀᴛᴇ: ${chat.detect ? 'ᴀᴄᴛɪᴠᴀᴛᴇᴅ' : 'ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ'}\n┗━━━━━━━━━━━━━━━━━━┛` 
+            }, { quoted: m });
         }
 
         if (command === 'setwelcome') {
-            if (!text) return m.reply('┃ ✎ ᴇʀʀᴏʀ: ɪɴɢʀᴇsᴀ ᴇʟ ᴛᴇxᴛᴏ.')
-            chat.customWelcome = text
-            m.reply(`┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ sᴛᴀᴛᴜs: ᴡᴇʟᴄᴏᴍᴇ ᴜᴘᴅᴀᴛᴇᴅ\n┗━━━━━━━━━━━━━━━━━━┛`)
+            if (!text) return m.reply('┃ ✎ ᴇʀʀᴏʀ: ɪɴɢʀᴇsᴀ ᴇʟ ɴᴜᴇᴠᴏ ᴛᴇxᴛᴏ.');
+            chat.customWelcome = text;
+            m.reply(`┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ ᴄᴏɴғɪɢ: ᴄᴜsᴛᴏᴍ ᴡᴇʟᴄᴏᴍᴇ sᴇᴛ\n┗━━━━━━━━━━━━━━━━━━┛`);
         }
 
         if (command === 'delwelcome') {
-            chat.customWelcome = ''
-            m.reply(`┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ sᴛᴀᴛᴜs: ᴡᴇʟᴄᴏᴍᴇ ʀᴇsᴇᴛᴛᴇᴅ\n┗━━━━━━━━━━━━━━━━━━┛`)
+            chat.customWelcome = '';
+            m.reply(`┏━━━〔 sʏsᴛᴇᴍ 〕━━━┓\n┃ ✎ ᴄᴏɴғɪɢ: ᴡᴇʟᴄᴏᴍᴇ ʀᴇsᴇᴛᴛᴇᴅ\n┗━━━━━━━━━━━━━━━━━━┛`);
         }
     }
 }
 
-export default groupConfig
+export default groupConfig;

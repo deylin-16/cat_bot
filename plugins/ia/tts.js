278f8c6181f7173bd578198005ba7dea73ef9402
deylin-16 <deylin1616@gmail.com>
2026-02-05 20:05:13 -0600

    Actualizar tts.js

--- plugins/ia/tts.js
+++ plugins/ia/tts.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 const ttsCommand = {
     name: 'tts',
@@ -6,29 +6,23 @@ const ttsCommand = {
     category: 'tools',
     run: async (m, { conn, text }) => {
         if (!text) return;
-
         try {
             await m.react('🗣️');
-
+            
             const url = `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(text)}&tl=es&client=tw-ob`;
 
-            
-            const response = await axios.get(url, {
-                responseType: 'arraybuffer',
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-                }
-            });
+            const res = await fetch(url);
+            const buffer = await res.buffer(); 
 
             await conn.sendMessage(m.chat, { 
-                audio: response.data, 
-                mimetype: 'audio/mpeg'//,
-               // ptt: false // Opcional: lo envía como nota de voz
+                audio: buffer, 
+                mimetype: 'audio/mpeg',
+                ptt: true 
             }, { quoted: m });
 
             await m.react('✅');
-        } catch (error) {
-            console.error(error);
+        } catch (err) {
+            console.error(err);
             await m.react('❌');
         }
     }

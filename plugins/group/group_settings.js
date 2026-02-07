78e1fb8ec5ccf97755a027629b5d261d5020a1bb
deylin-16 <deylin1616@gmail.com>
2026-02-07 13:59:33 -0600

    Actualizar group_settings.js

--- plugins/group/group_settings.js
+++ plugins/group/group_settings.js
@@ -6,53 +6,54 @@ const groupConfig = {
     category: 'group',
     admin: true,
     group: true,
-    run: async (m, { conn, text, command, participants, usedPrefix, isBotAdmin }) => {
+    run: async (m, { conn, text, command, participants, isBotAdmin }) => {
+        if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {}
         const chat = global.db.data.chats[m.chat]
 
         if (command === 'welcome') {
             chat.welcome = !chat.welcome
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴇsᴛᴀᴅᴏ: ʙɪᴇɴᴠᴇɴɪᴅᴀ\n┃ ✎ sᴛᴀᴛᴜs: ${chat.welcome ? 'ᴀᴄᴛɪᴠᴀᴅᴏ' : 'ᴅᴇsᴀᴄᴛɪᴠᴀᴅᴏ'}\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴇsᴛᴀᴅᴏ: ʙɪᴇɴᴠᴇɴɪᴅᴀ\n> ┃ ✎ sᴛᴀᴛᴜs: ${chat.welcome ? 'ᴀᴄᴛɪᴠᴀᴅᴏ' : 'ᴅᴇsᴀᴄᴛɪᴠᴀᴅᴏ'}\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (command === 'detect') {
             chat.detect = !chat.detect
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴇsᴛᴀᴅᴏ: ᴅᴇᴛᴇᴄᴛᴏʀ\n┃ ✎ sᴛᴀᴛᴜs: ${chat.detect ? 'ᴀᴄᴛɪᴠᴀᴅᴏ' : 'ᴅᴇsᴀᴄᴛɪᴠᴀᴅᴏ'}\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴇsᴛᴀᴅᴏ: ᴅᴇᴛᴇᴄᴛᴏʀ\n> ┃ ✎ sᴛᴀᴛᴜs: ${chat.detect ? 'ᴀᴄᴛɪᴠᴀᴅᴏ' : 'ᴅᴇsᴀᴄᴛɪᴠᴀᴅᴏ'}\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (command === 'setwelcome') {
-            if (!text) return m.reply('┏━━━〔 ᴇʀʀᴏʀ 〕━━━┓\n┃ ✎ ɪɴғᴏ: ɪɴɢʀᴇsᴀ ᴇʟ ᴛᴇxᴛᴏ\n┗━━━━━━━━━━━━━━━━━━┛')
+            if (!text) return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ɪɴɢʀᴇsᴀ ᴇʟ ᴛᴇxᴛᴏ.')
             chat.customWelcome = text
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴄᴏɴғɪɢ: ᴡᴇʟᴄᴏᴍᴇ sᴇᴛ\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴄᴏɴғɪɢ: ᴡᴇʟᴄᴏᴍᴇ sᴇᴛ\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (command === 'delwelcome') {
             chat.customWelcome = ''
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴄᴏɴғɪɢ: ᴡᴇʟᴄᴏᴍᴇ ʀᴇsᴇᴛ\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴄᴏɴғɪɢ: ᴡᴇʟᴄᴏᴍᴇ ʀᴇsᴇᴛ\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (/renombrar|setnombre|setname/i.test(command)) {
-            if (!isBotAdmin) return m.reply('┃ ✎ ᴇʀʀᴏʀ: ɴᴇᴄᴇsɪᴛᴏ sᴇʀ ᴀᴅᴍɪɴ.')
-            if (!text) return m.reply('┃ ✎ ɪɴғᴏ: ɪɴɢʀᴇsᴀ ᴇʟ ɴᴏᴍʙʀᴇ.')
+            if (!isBotAdmin) return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ɴᴇᴄᴇsɪᴛᴏ sᴇʀ ᴀᴅᴍɪɴ.')
+            if (!text) return m.reply('> ┃ ✎ ɪɴғᴏ: ɪɴɢʀᴇsᴀ ᴇʟ ɴᴏᴍʙʀᴇ.')
             await conn.groupUpdateSubject(m.chat, text)
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴄᴀᴍʙɪᴏ: ɴᴏᴍʙʀᴇ ᴀᴄᴛᴜᴀʟ\n┃ ✎ ᴠᴀʟᴜᴇ: ${text}\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴄᴀᴍʙɪᴏ: ɴᴏᴍʙʀᴇ ᴀᴄᴛᴜᴀʟ\n> ┃ ✎ ᴠᴀʟᴜᴇ: ${text}\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (/desc|setdesc/i.test(command)) {
-            if (!isBotAdmin) return m.reply('┃ ✎ ᴇʀʀᴏʀ: ɴᴇᴄᴇsɪᴛᴏ sᴇʀ ᴀᴅᴍɪɴ.')
+            if (!isBotAdmin) return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ɴᴇᴄᴇsɪᴛᴏ sᴇʀ ᴀᴅᴍɪɴ.')
             let newDesc = m.quoted ? m.quoted.text : text
-            if (!newDesc) return m.reply('┃ ✎ ɪɴғᴏ: ɪɴɢʀᴇsᴀ ʟᴀ ᴅᴇsᴄʀɪᴘᴄɪᴏɴ.')
+            if (!newDesc) return m.reply('> ┃ ✎ ɪɴғᴏ: ɪɴɢʀᴇsᴀ ʟᴀ ᴅᴇsᴄʀɪᴘᴄɪᴏɴ.')
             await conn.groupUpdateDescription(m.chat, newDesc)
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴄᴏɴғɪɢ: ᴅᴇsᴄ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴀ\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴄᴏɴғɪɢ: ᴅᴇsᴄ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴀ\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (/setfoto|setpp/i.test(command)) {
-            if (!isBotAdmin) return m.reply('┃ ✎ ᴇʀʀᴏʀ: ɴᴇᴄᴇsɪᴛᴏ sᴇʀ ᴀᴅᴍɪɴ.')
+            if (!isBotAdmin) return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ɴᴇᴄᴇsɪᴛᴏ sᴇʀ ᴀᴅᴍɪɴ.')
             let q = m.quoted ? m.quoted : m
             let mime = (q.msg || q).mimetype || ''
-            if (!/image/.test(mime)) return m.reply('┃ ✎ ᴇʀʀᴏʀ: ʀᴇsᴘᴏɴᴅᴇ ᴀ ᴜɴᴀ ɪᴍᴀɢᴇɴ.')
+            if (!/image/.test(mime)) return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ʀᴇsᴘᴏɴᴅᴇ ᴀ ᴜɴᴀ ɪᴍᴀɢᴇɴ.')
             let media = await q.download()
             await conn.updateProfilePicture(m.chat, media)
-            m.reply(`┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n┃ ✎ ᴄᴏɴғɪɢ: ғᴏᴛᴏ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴀ\n┗━━━━━━━━━━━━━━━━━━┛`)
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴄᴏɴғɪɢ: ғᴏᴛᴏ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴀ\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (/elimina|kick|ban|echar|sacar/i.test(command)) {
@@ -61,17 +62,17 @@ const groupConfig = {
             if (users.length === 0) return m.reply('> ✎ ɪɴғᴏ: ᴇᴛɪǫᴜᴇᴛᴀ ᴀ ᴀʟɢᴜɪᴇɴ.')
             for (let user of users) {
                 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-                m.reply(`┏━━━〔 ᴇxᴛᴇʀᴍɪɴᴀᴛᴇ 〕━━━┓\n┃ ✎ ᴜsᴇʀ: @${user.split('@')[0]}\n┃ ✎ sᴛᴀᴛᴜs: ʀᴇᴍᴏᴠᴇᴅ\n┗━━━━━━━━━━━━━━━━━━┛`, null, { mentions: [user] })
             }
+            return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴀᴄᴄɪᴏɴ: ᴜsᴜᴀʀɪᴏs ᴇʟɪᴍɪɴᴀᴅᴏs\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
         if (/tagall|todos|anuncio/i.test(command)) {
-            let txt = `┏━━━〔 ᴀɴᴜɴᴄɪᴏ ɢʀᴇᴘ 〕━━━┓\n┃ ✎ ᴍsɢ: ${text || 'sɪɴ ᴍᴏᴛɪᴠᴏ'}\n┃\n`
+            let txt = `> ┏━━━〔 ᴀɴᴜɴᴄɪᴏ ɢʀᴜᴘᴀʟ 〕━━━┓\n> ┃ ✎ ᴍsɢ: ${text || 'sɪɴ ᴍᴏᴛɪᴠᴏ'}\n> ┃\n`
             for (let mem of participants) {
-                txt += `┃ ✎ @${mem.id.split('@')[0]}\n`
+                txt += `> ┃ ✎ @${mem.id.split('@')[0]}\n`
             }
-            txt += `┗━━━━━━━━━━━━━━━━━━┛`
-            conn.sendMessage(m.chat, { text: txt, mentions: participants.map(a => a.id) })
+            txt += `> ┗━━━━━━━━━━━━━━━━━━┛`
+            return conn.sendMessage(m.chat, { text: txt, mentions: participants.map(a => a.id) })
         }
     }
 }

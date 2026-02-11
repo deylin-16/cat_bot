13ffabb9176e11d32d67f320b42cddf1bedba162
deylin-16 <deylin1616@gmail.com>
2026-02-11 13:29:49 -0600

    Actualizar prueba.js

--- plugins/tools/prueba.js
+++ plugins/tools/prueba.js
@@ -1,10 +1,13 @@
-import { translate } from '@vitalets/google-translate-api';
+import axios from 'axios';
 
 const translateConfig = {
     name: 'translate',
     alias: ['traducir', 'trt'],
     category: 'tools',
     run: async function (m, { text, args, command }) {
+        
+        const MyApiUrl = 'https://script.google.com/macros/s/AKfycbyrAxDGtL-_e8FjIJFIE4kdFK76jZ2rIIHfMtEcRwkdfX3Wz8JLOVfB4OHnCO6Dzism/exec';
+
         let lang = 'es';
         let targetText = text;
 
@@ -12,26 +15,23 @@ const translateConfig = {
             lang = args[0];
             targetText = args.slice(1).join(' ');
         }
-
-        if (!targetText && m.quoted && m.quoted.text) {
-            targetText = m.quoted.text;
-        }
-
-        if (!targetText) return m.reply(`> ✎ ɪɴғᴏ: ɪɴɢʀᴇsᴀ ᴇʟ ᴛᴇxᴛᴏ.\n> ᴜsᴏ: ${command} [iso] [texto]\n> ᴇᴊ: ${command} en hello`);
+        if (!targetText && m.quoted) targetText = m.quoted.text;
+        if (!targetText) return m.reply(`> ✎ ᴜsᴏ: ${command} [iso] [texto]`);
 
         try {
-            const result = await translate(targetText, { to: lang });
-            
-            let response = `> ┏━━━〔 ᴛʀᴀᴅᴜᴄᴄɪᴏɴ 〕━━━┓\n`;
-            response += `> ┃ ✎ ᴏʀɪɢᴇɴ: ${result.from.language.iso}\n`;
-            response += `> ┃ ✎ ᴅᴇsᴛɪɴᴏ: ${lang}\n`;
-            response += `> ┃ ✎ ʀᴇsᴜʟᴛᴀᴅᴏ: ${result.text}\n`;
-            response += `> ┗━━━━━━━━━━━━━━━━━━┛`;
-            
-            return m.reply(response);
+            const res = await axios.get(`${MyApiUrl}?text=${encodeURIComponent(targetText)}&to=${lang}`);
+            const data = res.data;
+
+            if (data.status) {
+                let response = `> ┏━━━〔 ᴛʀᴀᴅᴜᴄᴄɪᴏɴ ᴘʀɪᴠᴀᴅᴀ 〕━━━┓\n`;
+                response += `> ┃ ✎ ʀᴇsᴜʟᴛᴀᴅᴏ: ${data.result}\n`;
+                response += `> ┗━━━━━━━━━━━━━━━━━━┛`;
+                return m.reply(response);
+            } else {
+                throw new Error(data.error);
+            }
         } catch (e) {
-            console.error(e);
-            return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ᴇʟ sᴇʀᴠɪᴄɪᴏ ᴅᴇ ɢᴏᴏɢʟᴇ ᴇsᴛᴀ sᴀᴛᴜʀᴀᴅᴏ.');
+            return m.reply('> ┃ ✎ ᴇʀʀᴏʀ: ᴛᴜ ᴀᴘɪ ɴᴏ ʀᴇsᴘᴏɴᴅɪᴏ.');
         }
     }
 };

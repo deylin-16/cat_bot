1f0677cabffb5d9bf06484094e27a1eb4e785137
deylin-16 <deylin1616@gmail.com>
2026-02-11 12:57:45 -0600

    Actualizar group_settings.js

--- plugins/group/group_settings.js
+++ plugins/group/group_settings.js
@@ -8,14 +8,12 @@ const groupConfig = {
     group: true,
     botAdmin: true,
     run: async function (m, { conn, text, command, participants, chat }) {
-        
-        // Comando Welcome
+
         if (command === 'welcome') {
             chat.welcome = !chat.welcome
             return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴇsᴛᴀᴅᴏ: ʙɪᴇɴᴠᴇɴɪᴅᴀ\n> ┃ ✎ sᴛᴀᴛᴜs: ${chat.welcome ? 'ᴀᴄᴛɪᴠᴀᴅᴏ' : 'ᴅᴇsᴀᴄᴛɪᴠᴀᴅᴏ'}\n> ┗━━━━━━━━━━━━━━━━━━┛`)
         }
 
-        // Comando Detect
         if (command === 'detect') {
             chat.detect = !chat.detect
             return m.reply(`> ┏━━━〔 sɪsᴛᴇᴍᴀ 〕━━━┓\n> ┃ ✎ ᴇsᴛᴀᴅᴏ: ᴅᴇᴛᴇᴄᴛᴏʀ\n> ┃ ✎ sᴛᴀᴛᴜs: ${chat.detect ? 'ᴀᴄᴛɪᴠᴀᴅᴏ' : 'ᴅᴇsᴀᴄᴛɪᴠᴀᴅᴏ'}\n> ┗━━━━━━━━━━━━━━━━━━┛`)
